6a6dca2248f9bde77563f8f4e6980c2003a05d7b
Victor Mesquita <mesquitadev@gmail.com>
2025-01-22 07:56:14 -0300

    fi

--- core/configs.py
+++ core/configs.py
@@ -1,15 +1,17 @@
 import os
 from typing import ClassVar
 
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
 
+load_dotenv()
 class Settings(BaseSettings):
     """
     Configuracoes gerais usadas na aplicaco
     """
     API_V1_STR: str = "/api/v1"
-    DB_URL: str = "postgresql+asyncpg://doadmin:AVNS_uvj4yNFuuialz5-c_ju@db-cs-do-user-2961515-0.c.db.ondigitalocean.com:25060/geobee"
+    DB_URL: str = os.getenv("DB_URL")
     DBBaseModel: ClassVar = declarative_base()
 
 
